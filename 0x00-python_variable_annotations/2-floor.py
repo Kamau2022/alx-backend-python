2f548bc5cad1d32eefd74bd6fb3248fcc2ad3de6
Benson Kamau <benkabenson@gmail.com>
2023-04-27 17:01:21 +0000

    well documented

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""a module that defines a type-annotated function"""
+
+
+def floor(n: float) -> int:
+    """a function that returns converts float to int
+    Args:
+        n: float
+    Returns:
+        n: integer
+    """
+
+    return int(n)
