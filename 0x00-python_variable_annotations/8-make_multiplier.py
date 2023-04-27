3a8f845f3344a98e17eeb9a94021ad575e4d6b31
Benson Kamau <benkabenson@gmail.com>
2023-04-27 22:46:38 +0000

    added a function

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,10 +2,11 @@
 """a module showing a type-annotated function"""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """a function that takes float as argument
-       Args:
-           multiplier: float
-       returns
-    """
-    return make_multiplier(multiplier)
+    """a function that takes float as argument"""
+
+    def multiply(b: float) -> float:
+        """a function that returns second argument"""
+        return multiplier * b
+    return multiply
