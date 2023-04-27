82b3eecc638374bbd7fead7e4a83ae2ee277bc71
Benson Kamau <benkabenson@gmail.com>
2023-04-27 17:17:51 +0000

    fixed

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""a module that defines a type-annotated function"""
+
+
+def to_str(n: float) -> str:
+    """a function that returns string representation
+       of the float
+    Args:
+        n: float
+    Returns:
+        n: str
+    """
+
+    return str(n)
