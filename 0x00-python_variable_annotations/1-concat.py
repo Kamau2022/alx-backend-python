80e8d7af0c0451e084877606115d79a8896804a9
Benson Kamau <benkabenson@gmail.com>
2023-04-27 16:05:50 +0000

    concatenated

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""a module that defines a type-annotated function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """a function that concatenates two strings
+    Args:
+        str1: float
+        str2: number
+    Returns:
+        str: str1 + str2
+    """
+
+    return str1 + str2
