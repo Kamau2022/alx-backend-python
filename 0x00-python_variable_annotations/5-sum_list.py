41988036b330f27770a68958ffada26006a5e00a
Benson Kamau <benkabenson@gmail.com>
2023-04-28 17:56:20 +0000

    used a for function

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 """a module that defines a type-annotated function"""
+from typing import List
 
-input_list: list[float]
-def sum_list(input_list: input_list) -> input_list:
-    """a function that which takes a list input_list of 
+
+def sum_list(input_list: List[float]) -> float:
+    """a function that which takes a list input_list of
        floats as argument
     Args:
        input_list(float): list
     Returns:
         sum: float
     """
-    return [i + input_list for i in input_list]
+    x = 0
+    for item in input_list:
+        x += item
+    return x
