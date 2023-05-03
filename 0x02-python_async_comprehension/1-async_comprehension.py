090f760a1e6df13176ea6b9a1fed49f692ab274e
Benson Kamau <benkabenson@gmail.com>
2023-05-03 19:13:18 +0000

    second task

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" a module on Async Comprehension
+"""
+import random
+import asyncio
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """a coroutine that uses async comprehensing
+    """
+    values = [item async for item in async_generator()]
+    return values
