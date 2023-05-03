4dd419647e8ed2c1fc73c73aa03b8b9b71879280
Benson Kamau <benkabenson@gmail.com>
2023-05-03 19:29:09 +0000

    second task

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
 """
 import random
 import asyncio
-from typing import Iterator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Iterator[float]:
+async def async_comprehension() -> List[float]:
     """a coroutine that uses async comprehensing
     """
     values = [item async for item in async_generator()]
