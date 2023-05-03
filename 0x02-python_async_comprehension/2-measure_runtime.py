b7da716ca7f2d496fe55173e623c1ce19026d7af
Benson Kamau <benkabenson@gmail.com>
2023-05-03 20:14:28 +0000

    tasks completed

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""a module on Async Comprehension"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """ coroutine that will execute async_comprehension
+        four times in parallel using asyncio.gather
+    """
+    time_start = time.time()
+    await asyncio.gather(*[async_comprehension()
+                         for x in range(4)])
+    time_end = time.time()
+    duration = (time_end - time_start)
+    return duration
