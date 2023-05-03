0840ffa19d610bcbb1b1dafff7f04279f4784664
Benson Kamau <benkabenson@gmail.com>
2023-05-03 18:19:13 +0000

    first task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,13 +3,12 @@
 """
 import random
 import asyncio
-from typing import List
 
 
 async def async_generator() -> float:
     """a coroutine that takes argument
     """
     for i in range(10):
-        value = random.uniform(0, 10)
         await asyncio.sleep(1)
+        value = random.uniform(0, 10)
         yield value
